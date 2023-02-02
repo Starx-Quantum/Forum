b1efbdc42aeeb40c630185fc2ff06c6b68066aa4
Aman Kumar Pandey <113250329+Starx-Quantum@users.noreply.github.com>
2023-02-02 12:49:14 +0530

    Add files via upload

---
+++ admin/footer.php
@@ -0,0 +1,19 @@
+<div class="cleared"></div><div class="art-Footer">
+                    <div class="art-Footer-inner">
+                        <a href="#" class="art-rss-tag-icon" title="RSS"></a>
+                        <div class="art-Footer-text">
+                            <p><a href="../contact.php">Contact Us</a> | <a href="../terms.php">Terms of Use</a> |                               <a href="../prvs.php">Privacy Statement</a>|<a href="isuser.php">Online User</a><br />
+                                Copyright &copy; 2012 ---. All Rights Reserved.</p>
+                        </div>
+                    </div>
+                    <div class="art-Footer-background"></div>
+                </div>
+            </div>
+        </div>
+        
+    
+</body>
+
+
+
+</html>
---
+++ admin/home.php
@@ -0,0 +1,11 @@
+<?php 
+	session_start();
+	require("header.php");
+	require("checkUser.php");
+?>
+<script type="text/javascript">
+	document.getElementById("ahome").className="active";
+</script>
+
+
+<?php require("footer.php");?>
